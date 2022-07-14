6769c1a8e07cfa2a8535d18147a86e6fa98e3e4d
Al-khali <aourik.khalid@hotmail.com>
2022-07-14 21:42:01 +0200

    update stocks.py

--- stocks.py
+++ stocks.py
@@ -40,3 +40,15 @@ stock_ticker = yf.Ticker(stocklist)
 stock_info = stock_ticker.info
 stock_name = stock_info["shortName"]
 df = yf.download(stocklist,start= start_date,end= end_date, progress=False)
+
+# Candlestick Chart
+fig = go.Figure(data=[go.Candlestick(x=df.index,
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'])])
+fig.update_layout(xaxis_rangeslider_visible=False)
+fig.show()
+
+
+
